9f1b80b3b1b30f1d0f0a943bacc00823e7a9e886
huyuanixn325 <huyuanxin0325@163.com>
2019-08-22 11:13:13 +0800

    finish countProducts

--- __tests__/main-spec.js
+++ __tests__/main-spec.js
@@ -1,5 +1,10 @@
-const add = require('../main');
+const {countProducts} = require('../main');
 
-it ('should add two numbers', () => {
-    expect(add(2, 3)).toBe(5);
+it ('should count codes', () => {
+    //given
+    const codes = ['0003','0003','0001'];
+    //when
+    const countedCodes = countProducts(codes);
+    //then
+    expect(countedCodes[0].count).toBe(2);
 });
--- main.js
+++ main.js
@@ -1,5 +1,33 @@
-function add (left, right) {
-    return left + right;
+const databases= [
+{"id": "0001", "name" : "Coca Cola", "price": 3},
+{"id": "0002", "name" : "Diet Coke", "price": 4},
+{"id": "0003", "name" : "Pepsi-Cola", "price": 5},
+{"id": "0004", "name" : "Mountain Dew", "price": 6},
+{"id": "0005", "name" : "Dr Pepper", "price": 7},
+{"id": "0006", "name" : "Sprite", "price": 8},
+{"id": "0007", "name" : "Diet Pepsi", "price": 9},
+{"id": "0008", "name" : "Diet Mountain Dew", "price": 10},
+{"id": "0009", "name" : "Diet Dr Pepper", "price": 11},
+{"id": "0010", "name" : "Fanta", "price": 12}
+];
+
+function countProducts (codes) {
+  let countCodes = [];
+  let codeObject = {};
+  for (let index = 0; index < codes.length; index++) {
+      if(codeObject[codes[index]]==null){
+          codeObject[codes[index]]=1;
+      }else{
+          codeObject[codes[index]]=codeObject[codes[index]]+1;
+      }
+      
+  }
+  for(key in codeObject){
+      countCodes.push({code:key,count:codeObject[key]})
+  }
+  return countCodes;
 }
 
-module.exports = add;
+module.exports = {
+    countProducts 
+};
